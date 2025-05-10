160d694a1505ffc4d5af34205f11eaa3f75c920f
bneill7 <149976886+bneill7@users.noreply.github.com>
2025-05-10 12:20:37 -0400

    Clarify login.js

--- files/theme/files/login.js
+++ files/theme/files/login.js
@@ -1,9 +1,18 @@
 const hoursCounter = document.getElementById("hours_counter");
 
 //Google Sheets update
+// THE API_KEY is linked to the Google Cloud Account. The NHS Google Account shuold have access to the Google Cloud
+// Project if the API_KEY needs to be updated (which it shouldn't unless something drastic changes)
 const API_KEY = 'AIzaSyBPWyQbAwL5AUWZVhAATQv7cA284rFTsw0';
+// The SHEET_ID links back to the specific Google Sheet. Make sure the Google Sheet is public to all with the link, 
+// and that the SHEET_ID is properly copied
 const SHEET_ID = '1ZWKIf6ResLpZHFr5PpgTdoWWog2dga7DkzogH4wyX10';
+// This variable does nothing
 const numberOfMembers = 2
+// This is the range that the API draws from the Google Sheet. If a group of members are struggling to login, and
+// they all are close to each other on the Google Sheet, the range may be too small. Check that the range encompasses
+// all members, and that the first names are found in the leftmost column, last names are in the middle column, and 
+// the ΣHours is the rightmost column.
 const RANGE = 'Sheet1!A1:C112';
 
 async function fetchSheetData()
